e4099b43da7dff3f3e19db968d77def7a2a90e97
dlcn <microccl@foxmail.com>
2022-11-07 18:35:29 +0800

    实现WebView基本效果

--- app/src/main/java/com/dlcn/web/MainActivity.java
+++ app/src/main/java/com/dlcn/web/MainActivity.java
@@ -1,13 +1,41 @@
 package com.dlcn.web;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.view.KeyEvent;
+import android.webkit.WebView;
+import android.widget.Toast;
 
+public class MainActivity extends AppCompatActivity {
+    // 程序入口
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 设置 WebView 内容
+        WebView web_view = new WebView(this);
+        web_view.getSettings().setJavaScriptEnabled(true);
+        web_view.loadUrl("https://github.com/dlcnx");
+        setContentView(web_view);
+    }
+
+    // 2s内双击退出
+    private long exitTime = 0;
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN) {
+            if ((System.currentTimeMillis() - exitTime) > 2000) {
+                Toast.makeText(getApplicationContext(), "再按一次退出程序", Toast.LENGTH_SHORT).show();
+                exitTime = System.currentTimeMillis();
+            } else {
+                finish();
+                System.exit(0);
+            }
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
