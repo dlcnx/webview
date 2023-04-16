f79bd5bfe5f8ec1e5a362e9363575c9dcf7a4a7f
dlcn <microccl@foxmail.com>
2023-04-16 18:06:27 +0800

    使用腾讯x5内核

--- app/src/main/java/com/dlcn/web/MainActivity.java
+++ app/src/main/java/com/dlcn/web/MainActivity.java
@@ -1,14 +1,19 @@
 package com.dlcn.web;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-
 import android.view.KeyEvent;
 import android.view.View;
-import android.webkit.WebChromeClient;
-import android.webkit.WebView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.tencent.smtt.sdk.QbSdk;
+import com.tencent.smtt.sdk.QbSdk.PreInitCallback;
+import com.tencent.smtt.sdk.WebChromeClient;
+import com.tencent.smtt.sdk.WebSettings;
+import com.tencent.smtt.sdk.WebView;
+import com.tencent.smtt.sdk.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
     // 程序入口
@@ -16,10 +21,31 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // TBS初始化
+        QbSdk.setDownloadWithoutWifi(true);
+        QbSdk.initX5Environment(this, new PreInitCallback() {
+            @Override
+            public void onCoreInitFinished() {}
+            @Override
+            public void onViewInitFinished(boolean isX5) {}
+        });
         // 设置 WebView 内容
         WebView web_view = new WebView(this);
-        web_view.getSettings().setJavaScriptEnabled(true);
+        web_view.setWebViewClient(new WebViewClient());
         web_view.setWebChromeClient(new WebChromeClient());
+
+        WebSettings webSetting = web_view.getSettings();
+        // 允许 js 运行
+        webSetting.setJavaScriptEnabled(true);
+        // 允许 file 协议跨域
+        webSetting.setAllowFileAccessFromFileURLs(true);
+        // 其他设置
+        webSetting.setAllowFileAccess(true);
+        webSetting.setSupportZoom(true);
+        webSetting.setDatabaseEnabled(true);
+        webSetting.setAllowFileAccess(true);
+        webSetting.setDomStorageEnabled(true);
+        // 加载并切换界面
         web_view.loadUrl("file:///android_asset/index.html");
         setContentView(web_view);
     }
