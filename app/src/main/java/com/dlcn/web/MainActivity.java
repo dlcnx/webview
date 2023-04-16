19eae79646c5cf769acaad0f638b5730b5b07d23
dlcn <microccl@foxmail.com>
2023-04-16 18:00:03 +0800

    沉浸式全屏

--- app/src/main/java/com/dlcn/web/MainActivity.java
+++ app/src/main/java/com/dlcn/web/MainActivity.java
@@ -5,6 +5,7 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import android.view.KeyEvent;
+import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.widget.Toast;
@@ -21,6 +22,24 @@ public class MainActivity extends AppCompatActivity {
         web_view.setWebChromeClient(new WebChromeClient());
         web_view.loadUrl("file:///android_asset/index.html");
         setContentView(web_view);
+    }
+
+    // 聚焦时隐藏状态栏与底部导航栏
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    // 稳定布局与Sticky Immersion模式
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                    // 全屏显示(隐藏状态栏)
+                    View.SYSTEM_UI_FLAG_FULLSCREEN |
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                    // 隐藏底部导航栏
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+        }
     }
 
     // 2s内双击退出
